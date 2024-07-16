abc6004d7d35b30bf4e958137ee9d9b93a406582
dianaveselkina <vess_di@mail.ru>
2024-07-16 11:25:34 +0300

    add diamond pink

--- src/components/GameBoard2.vue
+++ src/components/GameBoard2.vue
@@ -8,7 +8,24 @@
         >Начать игру</MyButton
       >
     </transition>
-
+    <div id="21" @click="emersion" class="diamond diamond1">
+      <form
+        @submit.prevent="submitForm"
+        class="conundrum"
+        v-show="showConundrum"
+      >
+        <div class="conundrum__img"></div>
+        <label>Ответ</label>
+        <input v-model="answer" type="text" placeholder="Ваш ответ" />
+      </form>
+    </div>
+    <div id="22" @click="emersion" class="diamond diamond2"></div>
+    <div id="23" @click="emersion" class="diamond diamond3"></div>
+    <div id="24" @click="emersion" class="diamond diamond4"></div>
+    <div id="25" @click="emersion" class="diamond diamond5"></div>
+    <div id="24" @click="emersion" class="diamond diamond4"></div>
+    <div id="26" @click="emersion" class="diamond diamond6"></div>
+    <div id="27" @click="emersion" class="diamond diamond7"></div>
     <div class="diamond-container">
       <img
         v-for="diamond in diamonds"
@@ -27,6 +44,11 @@ import { ref } from 'vue';
 import RulesDiv2 from './RulesDiv2.vue';
 import MyButton from './MyButton.vue';
 const show = ref(false);
+const showConundrum = ref(false);
+const emersion = () => {
+  showConundrum.value = !showConundrum.value;
+};
+
 const diamonds = ref([
   {
     id: 1,
@@ -76,10 +98,82 @@ const diamonds = ref([
   flex-direction: column;
   margin: 35px auto 50px;
 }
+.conundrum {
+  width: 600px;
+  height: 700px;
+  position: relative;
+  z-index: 10;
+  display: flex;
+  flex-direction: column;
+  margin: 35px auto 50px;
+}
+.conundrum__img {
+  background-image: url(/public/img/conundrum1.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 550px;
+  height: 400px;
+}
 .start {
   display: flex;
   flex-direction: column;
   margin: 0 auto;
+}
+.diamond {
+  position: absolute;
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  cursor: pointer;
+}
+.diamond1 {
+  background-image: url(/public/img/diamond2.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  top: 366px;
+  left: 389px;
+}
+.diamond2 {
+  background-image: url(/public/img/diamond2.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  top: 620px;
+  left: 526px;
+}
+.diamond3 {
+  background-image: url(/public/img/diamond2.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  top: 530px;
+  left: 1160px;
+}
+.diamond4 {
+  background-image: url(/public/img/diamond2.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  top: 1066px;
+  left: 106px;
+}
+.diamond5 {
+  background-image: url(/public/img/diamond2.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  top: 602px;
+  left: 76px;
+}
+.diamond6 {
+  background-image: url(/public/img/diamond2.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  top: 1042px;
+  left: 1216px;
+}
+.diamond7 {
+  background-image: url(/public/img/diamond2.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  top: 0px;
+  left: 732px;
 }
 .diamond-container {
   border: solid 4px #eef13c;
